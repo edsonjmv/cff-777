62e2c16776cf088d8bef85114f392b835f98d951
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2017-12-14 12:32:09 +0100

    Improving login template

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
@@ -22,10 +22,38 @@ export class LoginComponent implements OnInit {
     photo: ''
   };
 
-  constructor(public _authF: AngularFireAuthModule, private _router: Router, public _authService: AuthService) {
+  @ViewChild('loginTab') logintab: ElementRef;
+  @ViewChild('signupTab') signuptab: ElementRef;
+  @ViewChild('loginContainer') loginContainer: ElementRef;
+  @ViewChild('signupContainer') signupContainer: ElementRef;
+
+  constructor(public _authF: AngularFireAuthModule, private _renderer: Renderer2, private _router: Router, public _authService: AuthService) {
   }
 
   ngOnInit() {
+  }
+
+  clickTabLogin() {
+    const tabActived = this.logintab.nativeElement;
+    const tabDisabled = this.signuptab.nativeElement;
+    const containerShowed = this.loginContainer.nativeElement;
+    const containerHided = this.signupContainer.nativeElement;
+    this.clickTab(tabActived, tabDisabled, containerShowed, containerHided);
+  }
+
+  clickTabSignup() {
+    const tabActived = this.signuptab.nativeElement;
+    const tabDisabled = this.logintab.nativeElement;
+    const containerShowed = this.signupContainer.nativeElement;
+    const containerHided = this.loginContainer.nativeElement;
+    this.clickTab(tabActived, tabDisabled, containerShowed, containerHided);
+  }
+
+  clickTab(tabActived, tabDisabled, containerShowed, containerHided) {
+    this._renderer.removeClass(tabDisabled, 'current');
+    this._renderer.removeClass(containerHided, 'show');
+    this._renderer.addClass(tabActived, 'current');
+    this._renderer.addClass(containerShowed, 'show');
   }
 
   signInWithTwitter() {
@@ -51,5 +79,117 @@ export class LoginComponent implements OnInit {
         console.log(error);
       });
   }
+
+  /*
+
+  var LoginModalController = {
+    tabsElementName: ".logmod__tabs li",
+    tabElementName: ".logmod__tab",
+    inputElementsName: ".logmod__form .input",
+    hidePasswordName: ".hide-password",
+
+    inputElements: null,
+    tabsElement: null,
+    tabElement: null,
+    hidePassword: null,
+
+    activeTab: null,
+    tabSelection: 0, // 0 - first, 1 - second
+
+    findElements: function () {
+        var base = this;
+
+        base.tabsElement = $(base.tabsElementName);
+        base.tabElement = $(base.tabElementName);
+        base.inputElements = $(base.inputElementsName);
+        base.hidePassword = $(base.hidePasswordName);
+
+        return base;
+    },
+
+    setState: function (state) {
+    	var base = this,
+            elem = null;
+
+        if (!state) {
+            state = 0;
+        }
+
+        if (base.tabsElement) {
+        	elem = $(base.tabsElement[state]);
+            elem.addClass("current");
+            $("." + elem.attr("data-tabtar")).addClass("show");
+        }
+
+        return base;
+    },
+
+    getActiveTab: function () {
+        var base = this;
+
+        base.tabsElement.each(function (i, el) {
+           if ($(el).hasClass("current")) {
+               base.activeTab = $(el);
+           }
+        });
+
+        return base;
+    },
+
+    addClickEvents: function () {
+    	var base = this;
+
+        base.hidePassword.on("click", function (e) {
+            var $this = $(this),
+                $pwInput = $this.prev("input");
+
+            if ($pwInput.attr("type") == "password") {
+                $pwInput.attr("type", "text");
+                $this.text("Hide");
+            } else {
+                $pwInput.attr("type", "password");
+                $this.text("Show");
+            }
+        });
+
+        base.tabsElement.on("click", function (e) {
+            var targetTab = $(this).attr("data-tabtar");
+
+            e.preventDefault();
+            base.activeTab.removeClass("current");
+            base.activeTab = $(this);
+            base.activeTab.addClass("current");
+
+            base.tabElement.each(function (i, el) {
+                el = $(el);
+                el.removeClass("show");
+                if (el.hasClass(targetTab)) {
+                    el.addClass("show");
+                }
+            });
+        });
+
+        base.inputElements.find("label").on("click", function (e) {
+           var $this = $(this),
+               $input = $this.next("input");
+
+            $input.focus();
+        });
+
+        return base;
+    },
+
+    initialize: function () {
+        var base = this;
+
+        base.findElements().setState().getActiveTab().addClickEvents();
+    }
+};
+
+$(document).ready(function() {
+    LoginModalController.initialize();
+});
+
+*/
 
 }
